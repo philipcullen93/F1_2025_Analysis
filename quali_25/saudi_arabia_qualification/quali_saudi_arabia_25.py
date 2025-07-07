e22554fa1ef01bc0bc51b3040c348b92c6046f09
Philip Cullen <philip.cullen93@gmail.com>
2025-07-08 00:48:52 +0100

    Created and populated Saudi Arabia qualification folder.

---
+++ quali_25/saudi_arabia_qualification/quali_saudi_arabia_25.py
@@ -0,0 +1,59 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+from timple.timedelta import strftimedelta
+
+
+import fastf1
+import fastf1.plotting
+from fastf1.core import Laps
+
+# Enable Matplotlib patches for plotting timedelta values
+fastf1.plotting.setup_mpl(mpl_timedelta_support=True, misc_mpl_mods=False,
+                          color_scheme=None)
+
+
+session = fastf1.get_session(2025, 'Saudi Arabia Grand Prix', 'Q')
+session.load()
+
+drivers = pd.unique(session.laps['Driver'])
+
+list_fastest_laps = []
+for drv in drivers:
+    drvs_fastest_lap = session.laps.pick_drivers(drv).pick_fastest()
+    if drvs_fastest_lap is not None:
+        list_fastest_laps.append(drvs_fastest_lap)
+fastest_laps = Laps(list_fastest_laps) \
+    .sort_values(by='LapTime') \
+    .reset_index(drop=True)
+
+pole_lap = fastest_laps.pick_fastest()
+fastest_laps['LapTimeDelta'] = fastest_laps['LapTime'] - pole_lap['LapTime']
+
+with open("fastest_laps_saudi_arabia_quali_25.txt", "w") as f:
+    print(fastest_laps[['Driver', 'LapTime', 'LapTimeDelta']], file=f)
+
+team_colors = list()
+for index, lap in fastest_laps.iterlaps():
+    color = fastf1.plotting.get_team_color(lap['Team'], session=session)
+    team_colors.append(color)
+
+fig, ax = plt.subplots()
+ax.barh(fastest_laps.index, fastest_laps['LapTimeDelta'],
+        color=team_colors, edgecolor='grey')
+ax.set_yticks(fastest_laps.index)
+ax.set_yticklabels(fastest_laps['Driver'])
+
+# show fastest at the top
+ax.invert_yaxis()
+
+# draw vertical lines behind the bars
+ax.set_axisbelow(True)
+ax.xaxis.grid(True, which='major', linestyle='--', color='black', zorder=-1000)
+
+lap_time_string = strftimedelta(pole_lap['LapTime'], '%m:%s.%ms')
+
+plt.suptitle(f"{session.event['EventName']} {session.event.year} Qualifying\n"
+             f"Fastest Lap: {lap_time_string} ({pole_lap['Driver']})")
+plt.savefig("Saudi Arabia Qualifying Results.png", dpi=300)
+plt.show()
